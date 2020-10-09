b8903d9b1b4ab28990be58471c09a9b53a46a039
Charles <charlesmorris172@gmail.com>
2020-10-09 12:28:44 +0100

    Added Media Queries

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const Joke = ({setup, punchline}) => {
-    return (
-        <div>
-            <p className="setup">{setup}</p>
-            <p className="punchline">{punchline}</p>
-        </div>
-    )
-}
+const Joke = ({ setup, punchline }) => {
+	return (
+		<Fragment>
+			<p className='setup'>{setup}</p>
+			<p className='punchline'>{punchline}</p>
+		</Fragment>
+	);
+};
 
 Joke.propTypes = {
-    setup: PropTypes.string.isRequired,
-    punchline: PropTypes.string.isRequired
-}
+	setup: PropTypes.string.isRequired,
+	punchline: PropTypes.string.isRequired,
+};
 
 export default Joke;
